4b30655304ab63160e6d3d3ecef92ee763d1585a
hperez29 <perezherbie29@yahoo.com>
2025-04-14 15:01:48 -0400

    desired inputExample

---
+++ wb1-demo1/src/main/java/com/pluralsight/inputExample.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+public class inputExample {
+
+    public static void main(String[] args) {
+
+    double number1 = 4;
+    double number2 = 5;
+
+    double product = number1 * number2;
+
+    System.out.println("The product of" + number1 + " and " + number2 + " is " + product );
+
+    }
+}
+
+
+
+
+
+
